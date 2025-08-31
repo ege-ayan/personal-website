3324a8b47b9e3cce19d3617a60678b8655250b6b
Ege Ayan <egeayan2478@gmail.com>
2025-08-31 18:40:43 +0300

    fix build issue

--- contents/blogs.ts
+++ contents/blogs.ts
@@ -10,14 +10,4 @@ export const blogs = [
     tags: ["Programming", "Software Development"],
     image: "/images/blogs/tactical-vs-strategic-programming.jpg",
   },
-  {
-    title: "Breaking Change: How Next.js 'Revolutionized' Web Development",
-    description:
-      "In each major release of Next.js, there are breaking changes that can be challenging to adapt to. This article will guide you through the process of updating your codebase to the latest version of Next.js.",
-    readTime: "10 min",
-    publishedAt: "2025-08-31",
-    href: "/blogs/nextjs-breaking-change",
-    tags: ["Next.js", "React.js", "Web Development"],
-    image: "/images/blogs/nextjs-breaking-change.jpg",
-  },
 ];
